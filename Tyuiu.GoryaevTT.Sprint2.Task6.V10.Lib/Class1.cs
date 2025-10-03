0c283f424b989a94bf469e83498d94e5773866a4
GoryaevTagir <Lobstera134@gmail.com>
2025-10-03 12:52:50 +0000

    Task 6

---
+++ Tyuiu.GoryaevTT.Sprint2.Task6.V10.Lib/Class1.cs
@@ -0,0 +1,85 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.GoryaevTT.Sprint2.Task6.V10.Lib
+{
+    public class Class1 : ISprint2Task5V10
+    {
+        public string FindDateOfPreviousDay(int g, int m, int n)
+        {
+            string ans = $"{n}.{m}.{g}";
+            if ((m == 2) || (m == 4) || (m == 6) || (m == 8) || (m == 9) || (m == 11))
+            {
+                switch (n)
+                {
+                    case 1:
+                        n = 31;
+                        m = m - 1;
+                        break;
+                    default:
+                        n = n - 1;
+                        break;
+
+                }
+            }
+            else if (m == 3)
+            {
+                switch (n)
+                {
+                    case 1:
+                        n = 28;
+                        m = m - 1;
+                        break;
+                    default:
+                        n = n - 1;
+                        break;
+
+                }
+            }
+            else if ((m == 1))
+            {
+                switch (n)
+                {
+                    case 1:
+                        n = 31;
+                        m = 12;
+                        g = g - 1;
+                        break;
+                    default:
+                        n = n - 1;
+                        break;
+                }
+            }
+            else
+            {
+                switch (n)
+                {
+                    case 1:
+                        n = 30;
+                        m = m - 1;
+                        break;
+                    default:
+                        n = n - 1;
+                        break;
+                }
+            }
+
+
+            if ((m >= 1) && (m <= 9) && (n >= 1) && (n <= 9))
+            {
+                return $"0{n}.0{m}.{g}";
+            }
+            else if ((m >= 1) && (m <= 9))
+            {
+                return $"{n}.0{m}.{g}";
+            }
+            else if ((n >= 1) && (n <= 9))
+            {
+                return $"0{n}.{m}.{g}";
+            }
+            else
+            {
+                return $"{n}.{m}.{g}";
+            }
+        }
+    }
+}
+
---
+++ Tyuiu.GoryaevTT.Sprint2.Task6.V10/Program.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.GoryaevTT.Sprint2.Task6.V10.Lib;
+namespace Tyuiu.GoryaevTT.Sprint2.Task6.V10
+{
+    class Program 
+        {
+        public static void Main(string[] args)
+        {
+            Class1 ds = new Class1();
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+            int z = Convert.ToInt32(Console.ReadLine());
+            string res = ds.FindDateOfPreviousDay(z, y, x);
+            Console.WriteLine(res);
+        }
+        }
+    }
+}
